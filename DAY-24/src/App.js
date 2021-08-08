125147794e689f0536d2b2383b4805d6eeafe637
ridz0326 <79255865+ridz0326@users.noreply.github.com>
2021-08-08 21:21:19 +0530

    THA 24 Updated

--- DAY-24/src/App.js
+++ DAY-24/src/App.js
@@ -1,27 +1,63 @@
 import { Route, Switch } from 'react-router-dom';
+import React from 'react';
 import './App.css';
-import Menu from './Components/Menu';
+import PrivateRoute from './Components/PrivateRoute'
+import Nav from './Components/Nav';
 import Home from './Components/Home';
 import About from './Components/About';
 import Profile from './Components/Profile';
 import Dashboard from './Components/Dashboard';
+import LoginContextProvider from './Components/LoginContext';
 
 
 
 function App() {
+
+  // const [login, setLogin] = useState(false);
+  // const [loading, setLoading] = useState(false);
+  // const Loading = () => {
+  //   return new Promise((resolve) => { setTimeout(resolve, 1000) })
+  // }
+
   return (
+    <LoginContextProvider>
     <div className="App">
       <>
-        <Menu/>
+        <Nav />
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route exact path='/profile' component={Profile} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/dashboard' component={Dashboard} />
+          <Route path='/about' component={About} />
+          <PrivateRoute path='/dashboard' component={Dashboard} />
+          <PrivateRoute path='/profile' component={Profile} />
+          {/* {login ? (
+            <>
+              <Route path='/dashboard' component={Dashboard} />
+              <Route path='/profile' component={Profile} />
+            </>
+          ) : (
+            <>
+              <Route path='/dashboard' component={Dashboard} > <Redirect to="/" /> </Route>
+              <Route path='/profile' component={Profile}> <Redirect to="/" /> </Route>
+            </>
+          )} */}
         </Switch>
+        {/* {loading ? "Loading..." :
+          <button 
+          onClick={() => {  
+            setLoading(!loading)
+            Loading().then(() => {
+              setLogin(!login)
+              setLoading(false)
+            }) }}
+            >
+            {login ? "Logout" : "Login"}</button>
+          } */}
       </>
     </div>
+    </LoginContextProvider>
   );
 }
+
+
 
 export default App;
--- DAY-24/src/Components/Home.jsx
+++ DAY-24/src/Components/Home.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import {LoginContext} from './LoginContext';
 
 const Home = () => {
+
+    const {login, setLogin, loading, setLoading} = useContext(LoginContext);
+
+    const Loading = () => {
+        return new Promise((resolve) => { setTimeout(resolve, 1000) })
+      }
+
     return(
         <>
         <h1>Home</h1>
         <p>If not logged in can't access, Profile & Dashboard</p>
+
+        {loading ? <h3>Loading...</h3> :
+          <button className="btn"
+            onClick={() => {  
+              setLoading(!loading) 
+              Loading().then(() => {
+                setLogin(!login)
+                setLoading(false)
+              }) }}
+              >
+                {login ? "Logout" : "Login"}</button>
+        }
         </>
     )
 }
